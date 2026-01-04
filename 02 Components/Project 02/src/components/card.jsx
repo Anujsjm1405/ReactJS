9943dec1ba06d24ff27fa91f7a23ee0fec50e34c
Anujsm1405 <anujsjm14@gmail.com>
2026-01-05 00:34:08 +0530

    Component

---
+++ 02 Components/Project 02/src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+              <div className="card">
+        <h1>I am Max</h1>
+      </div>
+    </div>
+  )
+}
+
+export default card
---
+++ 02 Components/Project 02/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+        <div className="navbar">
+            This is Navbar
+        </div>
+    </div>
+  )
+}
+
+export default navbar
